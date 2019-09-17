5b689055f65429b64741e0a07a49d2d4a3c1bc7e
Pragun Saini <pragunsaini117@gmail.com>
2019-09-17 23:22:50 +0530

    Setup Linting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>
-            <p>Hello World</p>
-            <p>WOW man</p>
-            <p>Miland</p>
-        </div>
+        <>
+            <div>CCPS - Excalibur</div>
+        </>
     )
 }
 
