ed8e38855d6bae348ed2bdf41b6b8140c3103095
Pragun Saini <pragunsaini117@gmail.com>
2019-09-20 13:31:18 +0530

    Added department

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { useState } from 'react'
 import './App.css'
 
-import 'tachyons'
-
 import Department from './Components/Department/Department'
 import Header from './Components/Header/Header'
 import Login from './Components/Login/Login'
 
-const App = () => {
-	const [route, setRoute] = useState('login')
-	const onRouteChange = newRoute => {
-		console.log(newRoute)
-		if (newRoute === 'login') setRoute('login')
-		else if (newRoute === 'student') setRoute('student')
-		else if (newRoute === 'department') setRoute('department')
-	}
-	console.log(route)
-	return (
-		<>
-			<Header />
+import Student from './components/Student'
 
-			{route === 'login' ? (
-				<Login onRouteChange={onRouteChange} />
-			) : (
-				<Department onRouteChange={onRouteChange} />
-			)}
-		</>
-	)
+import 'tachyons'
+
+const App = () => {
+    const [route, setRoute] = useState('login')
+    const onRouteChange = newRoute => {
+        console.log(newRoute)
+        if (newRoute === 'login') setRoute('login')
+        else if (newRoute === 'student') setRoute('student')
+        else if (newRoute === 'department') setRoute('department')
+    }
+    console.log(route)
+    return (
+        <>
+            <Header />
+
+            {route === 'login' ? (
+                <Login onRouteChange={onRouteChange} />
+            ) : route === 'student' ? (
+                <Student />
+            ) : (
+                <Department onRouteChange={onRouteChange} />
+            )}
+        </>
+    )
 }
 
 export default App
