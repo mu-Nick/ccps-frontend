8889818e816dbc249d77008857d3d035695b37e6
mu-Nick <anubhavsigh2690@gmail.com>
2019-09-27 11:53:41 +0530

    Connecting login to backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,18 @@ import React from 'react'
 import StudentLogin from './StudentLogin'
 import DepartmentLogin from './DepartmentLogin'
 
-const Login = ({ onRouteChange }) => {
+const Login = ({ onRouteChange, loadUser }) => {
 	return (
 		<div className=''>
 			<div class='mw9 center ph3-ns'>
 				<div class='cf ph2-ns'>
 					<div class='fl w-100 w-50-ns pa2'>
 						<div class='outline bg-white pv4'>
-							<StudentLogin className='fl w-50 pa2' onRouteChange={onRouteChange} />
+							<StudentLogin
+								className='fl w-50 pa2'
+								onRouteChange={onRouteChange}
+								loadUser={loadUser}
+							/>
 						</div>
 					</div>
 					<div class='fl w-100 w-50-ns pa2'>
@@ -18,6 +22,7 @@ const Login = ({ onRouteChange }) => {
 							<DepartmentLogin
 								className='fl w-50 pa2'
 								onRouteChange={onRouteChange}
+								loadUser={loadUser}
 							/>
 						</div>
 					</div>
