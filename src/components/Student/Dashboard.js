47a9c27420fa5740c09350cbb7ddd93e3a202862
Pragun Saini <pragunsaini117@gmail.com>
2019-09-27 17:42:36 +0530

    Added notifications

--- src/components/Student/Dashboard.js
+++ src/components/Student/Dashboard.js
@@ -1,47 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import styled from 'styled-components'
+import React from 'react'
 
-// const complaints = {
-//     Computer: ['Windows XP'],
-//     Hostel: ['Lack of cleanliness in hostel 10', 'High Mess bill in H-4']
-// }
+import { requestApproved } from '../../services/complaintsService'
 
-// const depts = ['Computer', 'Hostel']
-
-const Dashboard = ({ user }) => {
-    // const [departments, setDepartments] = useState(depts)
+const Dashboard = ({ user, notifications, setNotifications }) => {
+    const renderNotifications = () => {
+        return notifications.map(notification => (
+            <article key={notification.complaintID} className='dt w-100 pb2 mt2' href='#0'>
+                <div className='dtc v-mid pl3'>
+                    <h1 className='f6 f5-ns fw6 lh-title black mv0'>{notification.complaintID}</h1>
+                    <h2 className='f6 fw4 mt0 mb0 black-60'>{notification.message}</h2>
+                </div>
+                <div className='dtc v-mid'>
+                    <form className='w-100 tr'>
+                        <button
+                            type='button'
+                            className='f6 bg-white ba b--black-10 dim pointer pv1 black-60'
+                        >
+                            Support
+                        </button>
+                        <button
+                            type='button'
+                            className='f6 bg-white ba b--black-10 dim pointer pv1 black-60'
+                        >
+                            Decline
+                        </button>
+                    </form>
+                </div>
+            </article>
+        ))
+    }
 
     return (
         <>
             <div className='ma5'>
-                <div class='fl w-100 w-33-ns tc pv5 '>
+                <div className='fl w-100 w-33-ns tc pv5 '>
                     <h1>{user.id}</h1>
                 </div>
-                <div class='fl w-100 w-33-ns tc pv5'>
+                <div className='fl w-100 w-33-ns tc pv5'>
                     <h1>{user.name}</h1>
                 </div>
-                <div class='fl w-100 w-33-ns tc pv5'>
+                <div className='fl w-100 w-33-ns tc pv5'>
                     <h1>{user.email}</h1>
                 </div>
                 <div className='fl w-100 w-100-ns tc pv2'>
                     <h1>Notifications</h1>
                 </div>
-                <div className='fl w-100 w-100-ns tc pv2'>
-                    <article class='dt w-100 pb2 mt2' href='#0'>
-                        <div class='dtc v-mid pl3'>
-                            <h1 class='f6 f5-ns fw6 lh-title black mv0'>Subject</h1>
-                            <h2 class='f6 fw4 mt0 mb0 black-60'>Supporters</h2>
-                        </div>
-                        <div class='dtc v-mid'>
-                            <form class='w-100 tr'>
-                                <button class='f6 bg-white ba b--black-10 dim pointer pv1 black-60'>
-                                    View
-                                </button>
-                            </form>
-                        </div>
-                    </article>
-                </div>
+                <div className='fl w-100 w-100-ns tc pv2'>{renderNotifications()}</div>
             </div>
         </>
     )
