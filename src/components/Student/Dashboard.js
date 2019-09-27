a38e7f3182b4822aa7782499fe904d44e6138f15
mu-Nick <anubhavsigh2690@gmail.com>
2019-09-27 20:55:21 +0530

    adding student home styles

--- src/components/Student/Dashboard.js
+++ src/components/Student/Dashboard.js
@@ -14,34 +14,36 @@ const Dashboard = ({ user }) => {
 
     return (
         <>
-            <div className='ma5'>
-                <div class='fl w-100 w-33-ns tc pv5 '>
-                    <h1>{user.id}</h1>
+            <div className='db center mw5 tc black'>
+                <img
+                    className='db ba b--black-10'
+                    alt='Frank Ocean Blonde Album Cover'
+                    src='https://imageog.flaticon.com/icons/png/512/16/16480.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
+                />
+
+                <dl className='mt2 f6 lh-copy'>
+                    <dd className='ml0'>{user.id}</dd>
+                    <dd className='ml0 gray'>{user.name}</dd>
+                    <dd className='ml0 gray'>{user.email}</dd>
+                </dl>
+            </div>
+            <div>
+                <div className='center mw6'>
+                    <h1 className=''>Notifications</h1>
                 </div>
-                <div class='fl w-100 w-33-ns tc pv5'>
-                    <h1>{user.name}</h1>
-                </div>
-                <div class='fl w-100 w-33-ns tc pv5'>
-                    <h1>{user.email}</h1>
-                </div>
-                <div className='fl w-100 w-100-ns tc pv2'>
-                    <h1>Notifications</h1>
-                </div>
-                <div className='fl w-100 w-100-ns tc pv2'>
-                    <article class='dt w-100 pb2 mt2' href='#0'>
-                        <div class='dtc v-mid pl3'>
-                            <h1 class='f6 f5-ns fw6 lh-title black mv0'>Subject</h1>
-                            <h2 class='f6 fw4 mt0 mb0 black-60'>Supporters</h2>
+                <ul className='list pl0 mt0 measure center'>
+                    <li className='flex items-center lh-copy pa3 ph0-l bb b--black-10'>
+                        <div className='pl3 flex-auto'>
+                            <span className='f6 db black-70'>Subjet</span>
+                            <span className='f6 db black-70'>Supporters</span>
                         </div>
-                        <div class='dtc v-mid'>
-                            <form class='w-100 tr'>
-                                <button class='f6 bg-white ba b--black-10 dim pointer pv1 black-60'>
-                                    View
-                                </button>
-                            </form>
+                        <div>
+                            <button href='#0' className='f6 link blue hover-dark-gray'>
+                                Support
+                            </button>
                         </div>
-                    </article>
-                </div>
+                    </li>
+                </ul>
             </div>
         </>
     )
